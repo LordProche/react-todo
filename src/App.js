f204fe70d17b8940352195524fa4f12d9e446ec7
LordProche <48643054+LordProche@users.noreply.github.com>
2022-05-13 23:02:41 +0300

    Search form without logic and Input tasks form logic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import List from "./components/List/List";
 import InputForm from "./components/InputForm/InputForm";
+import {useDispatch} from "react-redux";
+import {addTask} from "./redux/tasksSlice";
 
 const App = () => {
+    const dispatch = useDispatch()
     return (
         <>
-            <InputForm/>
+            <InputForm
+                placeholder='Add task'
+                onEnter={(message) => dispatch(addTask(message))}
+                style={{marginTop: '40px'}}
+                withButton={true}
+            />
+            <InputForm placeholder='Search' onEnter={(message) => dispatch(addTask(message))} style={{marginTop: '10px'}}/>
             <List/>
         </>
     );
