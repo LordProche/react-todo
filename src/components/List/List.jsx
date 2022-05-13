c4519667829a4ae0a243d7ec09577810960a292d
LordProche <48643054+LordProche@users.noreply.github.com>
2022-05-13 23:43:03 +0300

    Info if todo list is empty

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -8,11 +8,17 @@ import styles from './List.module.css'
 const List = () => {
     const tasks = useSelector(state => state.tasksSlice.tasks)
     return (
-        <div className={styles.list}>
-            {tasks.map(task => (
-                <ListItem key={task.id} task={task}/>
-            ))}
-        </div>
+
+        tasks.length > 0 ?
+            <div className={styles.list}>
+                {tasks.map(task => (
+                    <ListItem key={task.id} task={task}/>
+                ))}
+            </div>
+            :
+            <p className={styles.empty_info}>Nothing here</p>
+
+
     );
 };
 
