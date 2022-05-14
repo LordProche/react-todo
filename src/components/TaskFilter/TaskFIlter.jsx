c6b8014903ffb8c68fdb7c0e5efb72a30d970849
LordProche <48643054+LordProche@users.noreply.github.com>
2022-05-14 20:15:00 +0300

    Task filter component

--- src/components/TaskFilter/TaskFIlter.jsx
+++ src/components/TaskFilter/TaskFIlter.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import Input from "../UI/Input/Input";
 import styles from "./TaskFilter.module.css";
+import {useDispatch, useSelector} from "react-redux";
+import {setFilterByTitle} from "../../redux/filtersSlice";
 
 const TaskFilter = () => {
+    const filterValue = useSelector(state => state.filtersSlice.filterByTitle)
+    const dispatch = useDispatch()
+
     return (
-        <Input className={styles.filter_input} placeholder='Search'/>
+        <Input
+            className={styles.filter_input}
+            placeholder='Search'
+            value={filterValue}
+            onChange={(e) => dispatch(setFilterByTitle(e.target.value))}
+        />
     );
 };
 
