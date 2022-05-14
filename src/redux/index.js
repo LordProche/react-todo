5e1d8552a4881a55cf032ae3673375e418e8d384
LordProche <48643054+LordProche@users.noreply.github.com>
2022-05-14 20:14:03 +0300

    Conntect filters slice

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,9 +1,11 @@
 import {configureStore} from "@reduxjs/toolkit";
 import tasksSlice from "./tasksSlice";
+import filtersSlice from "./filtersSlice";
 
 
 export const store = configureStore({
     reducer: {
-        tasksSlice
+        tasksSlice,
+        filtersSlice
     }
 })
