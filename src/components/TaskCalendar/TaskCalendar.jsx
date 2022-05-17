44c6265177788a50db4b22b96170aa1818c8ad98
LordProche <48643054+LordProche@users.noreply.github.com>
2022-05-18 00:00:23 +0300

    Change calendar animation

--- src/components/TaskCalendar/TaskCalendar.jsx
+++ src/components/TaskCalendar/TaskCalendar.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import styles from './TaskCalendar.module.css'
+import {useSelector} from "react-redux";
 
 const TaskCalendar = ({weekDay, day, isActive,  width, height, ...props}) => {
 
     const shortWeekDay = weekDay.substring(0, 2)
     const formattedDay = day < 10 ? `0${day}` : day;
+    const tasks = useSelector(state => state.tasksSlice.tasks)
+    const countOfTasksByDay = tasks.reduce((prev, current) => current.day === weekDay ? prev + 1 : prev, 0)
 
+    console.log(countOfTasksByDay)
     const calendarStyles = {
         width,
         height
@@ -15,6 +19,11 @@ const TaskCalendar = ({weekDay, day, isActive,  width, height, ...props}) => {
         <div className={ [styles.calendar, isActive ? styles.active : ''].join(' ')} style={calendarStyles} {...props}>
             <span className={styles.weekDay}>{shortWeekDay}</span>
             <span className={styles.day}>{formattedDay}</span>
+            {countOfTasksByDay ?
+                <p className={styles.count_of_posts}>{countOfTasksByDay}</p>
+                :
+                null
+            }
         </div>
     );
 };
