db4602ca4889d0373e449825b142a1462784f8e9
LordProche <48643054+LordProche@users.noreply.github.com>
2022-05-18 23:42:22 +0300

    Save data to localstorage with redux-persist

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,12 +1,42 @@
-import {configureStore} from "@reduxjs/toolkit";
+import {configureStore, combineReducers} from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER
+} from "redux-persist";
+import storage from 'redux-persist/lib/storage'
+
 import tasksSlice from "./tasksSlice";
 import filtersSlice from "./filtersSlice";
 import calendarSlice from './calendarSlice'
 
-export const store = configureStore({
-    reducer: {
-        tasksSlice,
-        filtersSlice,
-        calendarSlice
-    }
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const rootReducer = combineReducers({
+    tasksSlice,
+    filtersSlice,
+    calendarSlice
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+})
+
+export const persistor = persistStore(store)
+export default store
